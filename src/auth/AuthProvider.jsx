d5ff51efb3b70890216d0746d8df31b399ffe0cb
Moknine Elhadj <93493650+Edjemoknine@users.noreply.github.com>
2024-05-19 17:36:52 +0100

    Update AuthProvider.jsx

--- src/auth/AuthProvider.jsx
+++ src/auth/AuthProvider.jsx
@@ -3,8 +3,8 @@ export const AuthProvider = ({ children }) => (
   <KindeProvider
     clientId="aba1b093f743450cab4937d7f9d22d48"
     domain="https://moknine.kinde.com"
-    redirectUri="http://localhost:5173"
-    logoutUri="http://localhost:5173"
+    redirectUri="https://movico4u.netlify.app/"
+    logoutUri="https://movico4u.netlify.app/"
   >
     {children}
   </KindeProvider>
